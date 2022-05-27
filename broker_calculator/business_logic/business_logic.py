7fa8f69c3c33537a92b571d3239de6f3c9592dea
Kshitij Saxena <ksaxena@newstore.com>
2022-05-27 12:57:05 +0200

    Business logic handles not implemented brokers gracefully

--- broker_calculator/business_logic/business_logic.py
+++ broker_calculator/business_logic/business_logic.py
@@ -13,10 +13,13 @@ def get_brokerage_for_sip(broker_name, interval, start_date, end_date, amount, c
             except AttributeError as method_not_found:
                 logging.debug(method_not_found)
                 logging.error(f'broker {broker_name} should implement get_brokerage_for_sip function.')
+                raise NotImplementedError(f'method get_brokerage_for_sip not implemented in broker {broker_name}')
         except AttributeError as err:
             logging.debug(err)
             logging.error(f'Attr err: broker with name {broker_name} should be in package called {broker_name.lower()}.')
+            raise NotImplementedError(f'broker {broker_name} not found in the package {_module}')
     except ImportError:
         logging.error(f'Import err: you are trying to find class {broker_name} in package {_module} which does not '
                       f'exist.')
+        raise NotImplementedError(f'package {_module} not found')
 
--- broker_calculator/business_logic/tests/test_business_logic.py
+++ broker_calculator/business_logic/tests/test_business_logic.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import patch
 
+import pytest
+
 from broker_calculator.business_logic import business_logic
 
 
@@ -10,6 +12,10 @@ class MockBroker:
 
     def get_brokerage_for_sip(self, broker_name, interval, start_date, end_date, amount, currency):
         return self.brokerage
+
+
+class MockBrokerWithoutSipImplementation:
+    pass
 
 
 class TestGetBrokerageForSIP(unittest.TestCase):
@@ -31,4 +37,41 @@ class TestGetBrokerageForSIP(unittest.TestCase):
             amount=self.amount,
             currency=self.currency
         )
-        self.assertEqual(brokerage, 0.0)
+        self.assertEqual(0.0, brokerage)
+
+    def test_package_for_broker_does_exist(self):
+        with pytest.raises(NotImplementedError) as nie:
+            _ = business_logic.get_brokerage_for_sip(
+                broker_name='RandomBroker',
+                interval=self.interval,
+                start_date=self.start_date,
+                end_date=self.end_date,
+                amount=self.amount,
+                currency=self.currency
+            )
+        self.assertEqual('package broker_calculator.models.randombroker not found', str(nie.value))
+
+    def test_broker_not_found_in_package(self):
+        with pytest.raises(NotImplementedError) as nie:
+            _ = business_logic.get_brokerage_for_sip(
+                broker_name='BROKER',
+                interval=self.interval,
+                start_date=self.start_date,
+                end_date=self.end_date,
+                amount=self.amount,
+                currency=self.currency
+            )
+        self.assertEqual('broker BROKER not found in the package broker_calculator.models.broker', str(nie.value))
+
+    @patch('broker_calculator.models.broker.Broker', new=MockBrokerWithoutSipImplementation)
+    def test_broker_does_not_implement_get_brokerage_for_sip(self):
+        with pytest.raises(NotImplementedError) as nie:
+            _ = business_logic.get_brokerage_for_sip(
+                broker_name=self.broker_name,
+                interval=self.interval,
+                start_date=self.start_date,
+                end_date=self.end_date,
+                amount=self.amount,
+                currency=self.currency
+            )
+        self.assertEqual('method get_brokerage_for_sip not implemented in broker Broker', str(nie.value))
